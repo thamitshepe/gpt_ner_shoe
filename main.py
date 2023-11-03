861f6c4e1f42b0872988be2967726ea075e0a301
thamitshepe <ttshepe2002@gmail.com>
2023-11-03 08:43:43 +0200

    ye

--- main.py
+++ main.py
@@ -329,4 +329,4 @@ async def process_text(text: str = Form(...)):
     
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10007)
