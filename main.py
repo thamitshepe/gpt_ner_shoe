42659df992d4cd1fe45fe5ffbe82e0d46af3362f
Thami Tshepe <60922877+thamitshepe@users.noreply.github.com>
2023-11-04 07:56:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ langchain.llm_cache = UpstashRedisCache(redis_=Redis(url=URL, token=TOKEN))
 
 llm = ChatOpenAI(
     model_name="gpt-4",
-    temperature=1.2,
+    temperature=0.6,
     max_tokens=2008,
     openai_api_key=openai_api_key
 )
