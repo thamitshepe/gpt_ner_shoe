e0d72806c6b95cf2ffa88ed7a3e8862be51a60e5
Thami Tshepe <60922877+thamitshepe@users.noreply.github.com>
2023-11-21 06:29:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -61,8 +61,8 @@ sheet2 = workbook_sheet2.sheet1
 
 llm = ChatOpenAI(
     model_name="gpt-4",
-    temperature=1,
-    max_tokens=2500,
+    temperature=0.8,
+    max_tokens=2000,
     openai_api_key=openai_api_key
 )
 
