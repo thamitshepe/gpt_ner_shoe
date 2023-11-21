c56634db546a10d1399efb0fd7ca82771af0345f
Thami Tshepe <60922877+thamitshepe@users.noreply.github.com>
2023-11-21 04:57:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -404,13 +404,13 @@ def extract_and_store_data(text):
     spreadsheet_wholecell = gc.open('WholeCell Inventory Template')
 
     # Select the first worksheet in the 'WholeCell Inventory Template' sheet
-    worksheet_wholecell = spreadsheet_wholecell.get_worksheet(1)
+    worksheet_wholecell = spreadsheet_wholecell.get_worksheet(0)
 
     # Open the 'Product Catalog Template' sheet
     spreadsheet_catalog = gc.open('product catalog template')
 
     # Select the first worksheet in the 'Product Catalog Template' sheet
-    worksheet_catalog = spreadsheet_catalog.get_worksheet(1)
+    worksheet_catalog = spreadsheet_catalog.get_worksheet(0)
 
     # Get the column names from the first row of both worksheets
     header_row_wholecell = worksheet_wholecell.row_values(1)
@@ -423,7 +423,7 @@ def extract_and_store_data(text):
 
         # Define the keys you want in data_dict for each sheet
         keys_to_extract_wholecell = ['Model', 'Manufacturer', 'Cost', 'Price Paid', 'Sku', 'Grade', 'Damages', 'Capacity', 'Quantity', 'Complete']
-        keys_to_extract_catalog = ['Model', 'Code', 'Manufacturer', 'Capacity', 'Sku', 'Grade', 'Damages']
+        keys_to_extract_catalog = ['Model', 'Code', 'Manufacturer', 'Sku', 'Grade', 'Damages']
 
         # Iterate through the keys and add them to data_dict for each sheet if they exist in the product
         for key in keys_to_extract_wholecell:
